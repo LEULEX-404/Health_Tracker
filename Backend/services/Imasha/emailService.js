937cebe74b068db6f5d8a5e0b1127acdb1f63ce7
Miyuru Tharuka <miyurut20@gmail.com>
2026-03-26 07:39:51 +0530

    Mail service Fixed

--- Backend/services/Imasha/emailService.js
+++ Backend/services/Imasha/emailService.js
@@ -13,8 +13,11 @@ const createTransporter = () => {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
     },
+    tls: {
+      family: 4 // Force IPv4
+    }
   });
-};
+}
 
 /**
  * Sends email verification email
